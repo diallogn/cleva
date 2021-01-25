85f49d3f418a6f22319190c57b440e55c16c7752
Abdoulaye Diallo <dialloabdoulaye71@gmail.com>
2021-01-25 03:26:57 +0000

    reunitialize

--- app.js
+++ app.js
@@ -4,31 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-var mongoose = require('mongoose');
 
-var async = require('async');
-var bcryptjs = require('bcryptjs');
-var moment = require('moment');
-var debug = require('debug');
-var expressValidator = require('express-validator');
-var httpErrors = require('http-errors');
-var bodyParser = require('body-parser');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var localStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
-var membersRouter = require('./routes/api/members');
 
 var app = express();
 
-// Connect Database
-mongoose.connect('mongodb://localhost/login', {useNewUrlParser:true, useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -43,8 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/testAPI', testAPIRouter);
-app.use('/api/members', membersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
